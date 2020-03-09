960494d4f328e1d5c5b9f189391eced385baf231
Duncan <duncanmorrison2001@yahoo.com>
2020-03-09 16:39:44 +0000

    changed base URL for heroku

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'https://pacific-brook-60350.herokuapp.com/'
 });
 
 //local server url
 // baseURL: 'http://localhost:3000/',
-// heroku url: 'https://salty-harbor-93115.herokuapp.com/'
+// heroku url: 'https://pacific-brook-60350.herokuapp.com/'
 
 export default instance;
